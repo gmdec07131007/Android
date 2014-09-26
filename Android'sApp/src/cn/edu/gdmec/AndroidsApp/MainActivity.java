69f12383ea87b6be6022df72c19e137282111d77
gdmec07131027 <2778382859@qq.com>
2014-09-26 11:47:06 +0800

    第二次作业

---
+++ Android'sApp/src/cn/edu/gdmec/AndroidsApp/MainActivity.java
@@ -0,0 +1,73 @@
+package cn.edu.gdmec.AndroidsApp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.util.*;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "LIFECYCLE";
+    @Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.i(TAG, "The activity state---->onDestroy");
+	}
+
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.i(TAG, "The activity state---->onPause");
+	}
+
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.i(TAG, "The activity state---->onReatart"); 
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.i(TAG, "The activity state---->onResume");
+	}
+
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.i(TAG, "The activity state---->onStart");
+	}
+
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.i(TAG, "The activity state---->onStop");
+	}
+
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "this is a test string "); 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
